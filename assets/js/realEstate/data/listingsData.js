d9e17ba14ce92fcc263ee0323ea6b32c3542a104
bernscode <bern@bernscode.com>
2018-06-15 00:59:32 -0700

    Added nine listings into the data with diff images.

--- assets/js/realEstate/data/listingsData.js
+++ assets/js/realEstate/data/listingsData.js
@@ -16,5 +16,149 @@ var data = [
       'Swimming Pool',
       'Fireplace'
     ]
+  },
+  {
+    neighborhood: 'Los Angeles',
+    house-type: 'Houses',
+    address: '6231 Hacienda Pl',
+    city: 'Hollywood',
+    state: 'CA',
+    bedroom: 4,
+    bathroom: 3,
+    price: 850000,
+    square-feet: 3115,
+    lot-size: 4312,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxqB-6SvXoj8J4FVBn3t-FUQN9vGEb4p4eYUNgh2FLBQeJigIB'
+    features: [
+      'Garage',
+      'Swimming Pool',
+      'Fireplace'
+    ]
+  },
+  {
+    neighborhood: 'Los Angeles',
+    house-type: 'Houses',
+    address: '6231 Hacienda Pl',
+    city: 'Hollywood',
+    state: 'CA',
+    bedroom: 4,
+    bathroom: 3,
+    price: 850000,
+    square-feet: 3115,
+    lot-size: 4312,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa2wEJLGiGZIpL82s8J0yq6s7AfXgkZmTip7XnAQwOYVt3Afqx'
+    features: [
+      'Garage',
+      'Swimming Pool',
+      'Fireplace'
+    ]
+  },
+  {
+    neighborhood: 'Los Angeles',
+    house-type: 'Houses',
+    address: '6231 Hacienda Pl',
+    city: 'Hollywood',
+    state: 'CA',
+    bedroom: 4,
+    bathroom: 3,
+    price: 850000,
+    square-feet: 3115,
+    lot-size: 4312,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqc5eWMNEbkZYs1jQL-d1VJuOyVnGKZDsRWaHL7y-HSCvkruVL'
+    features: [
+      'Garage',
+      'Swimming Pool',
+      'Fireplace'
+    ]
+  },
+  {
+    neighborhood: 'Los Angeles',
+    house-type: 'Houses',
+    address: '6231 Hacienda Pl',
+    city: 'Hollywood',
+    state: 'CA',
+    bedroom: 4,
+    bathroom: 3,
+    price: 850000,
+    square-feet: 3115,
+    lot-size: 4312,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtu-X3dkZ9IxdHG88r7bHpTLOoxKmI1WaX1SMmiTrpnkt7-8th'
+    features: [
+      'Garage',
+      'Swimming Pool',
+      'Fireplace'
+    ]
+  },
+  {
+    neighborhood: 'Los Angeles',
+    house-type: 'Houses',
+    address: '6231 Hacienda Pl',
+    city: 'Hollywood',
+    state: 'CA',
+    bedroom: 4,
+    bathroom: 3,
+    price: 850000,
+    square-feet: 3115,
+    lot-size: 4312,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq4tXDkjsxTYdIuZf4Ov2Zjp5XVOW-QJsZ1-GxAlvA3Dn3kAexQg'
+    features: [
+      'Garage',
+      'Swimming Pool',
+      'Fireplace'
+    ]
+  },
+  {
+    neighborhood: 'Los Angeles',
+    house-type: 'Houses',
+    address: '6231 Hacienda Pl',
+    city: 'Hollywood',
+    state: 'CA',
+    bedroom: 4,
+    bathroom: 3,
+    price: 850000,
+    square-feet: 3115,
+    lot-size: 4312,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuDTe7KWulkTF9Yifx0VUAli_7iTcbtTDSUPE6OrucY68UfVjM'
+    features: [
+      'Garage',
+      'Swimming Pool',
+      'Fireplace'
+    ]
+  },
+  {
+    neighborhood: 'Los Angeles',
+    house-type: 'Houses',
+    address: '6231 Hacienda Pl',
+    city: 'Hollywood',
+    state: 'CA',
+    bedroom: 4,
+    bathroom: 3,
+    price: 850000,
+    square-feet: 3115,
+    lot-size: 4312,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuK3Sfp5J2xIhMa5qU9nzSLFdGQqSUEkkRWKoHCkltpkC7BtkPqg'
+    features: [
+      'Garage',
+      'Swimming Pool',
+      'Fireplace'
+    ]
+  },
+  {
+    neighborhood: 'Los Angeles',
+    house-type: 'Houses',
+    address: '6231 Hacienda Pl',
+    city: 'Hollywood',
+    state: 'CA',
+    bedroom: 4,
+    bathroom: 3,
+    price: 850000,
+    square-feet: 3115,
+    lot-size: 4312,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2fNSEbdycsKD2VZVRNTf6ckD1lYz0o8HTyhpXjGeGe0AsSGIH'
+    features: [
+      'Garage',
+      'Swimming Pool',
+      'Fireplace'
+    ]
   }
 ]
